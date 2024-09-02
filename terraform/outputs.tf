14d2ae55b4788f3a079460e2b864015370ae7d93
1AMTEDDY <petersonwoko@gmail.com>
2024-09-01 21:47:16 -0500

    adding outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "gke_cluster_name" {
+  value = module.gke.gke_cluster_name
+}
+
+output "gke_cluster_endpoint" {
+  value = module.gke.gke_cluster_endpoint
+}
+
+output "service_url" {
+  value = module.kubernetes.service_url
+}
