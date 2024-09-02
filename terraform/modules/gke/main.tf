4f0edcfca2cfc7ebc7164a39dcf174c6859d471e
1AMTEDDY <petersonwoko@gmail.com>
2024-09-02 10:41:39 -0500

    k8s 7

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -10,15 +10,10 @@ terraform {
   }
 }
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.default.endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.default.master_auth[0].cluster_ca_certificate,
-  )
-}
-data "google_container_cluster" "default" {
-  name       = var.cluster_name
-  depends_on = [module.gke]
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+
 }
 module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google"
