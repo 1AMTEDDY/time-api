577d0f5d17b1740c796af75d1609787929fe99d3
1AMTEDDY <petersonwoko@gmail.com>
2024-09-02 09:14:19 -0500

    k8s 1

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -1,11 +1,13 @@
-output "endpoint" {
-  value = google_container_cluster.primary.endpoint
+output "gke_cluster_name" {
+  value = module.gke.name
 }
 
-output "cluster_ca_certificate" {
-  value = base64encode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+output "gke_cluster_endpoint" {
+  value = module.gke.endpoint
 }
-
+output "gke_cluster_ca_certificate" {
+  value = module.gke.master_auth[0].cluster_ca_certificate
+}
 output "token" {
   value = data.google_client_config.default.access_token
 }
